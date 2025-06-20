0fb97f29008c6c3de82a3447a24730095b6493b1
YaelWil <yaelwilamo@gmail.com>
2025-06-20 11:10:15 +0300

    Initial table

---
+++ db/create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    uuid CHAR(36) PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    user_type VARCHAR(50) NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
